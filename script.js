50d00969e2acd103cd08fa461a1d717dad1c2db9
Ajs08 <alberth0804@hotmail.com>
2025-10-27 20:41:21 -0500

    Simple calculations added, missing zero validation and other operator buttons

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
-const resultScreen = document.querySelector("#calculator-screen");
+const screen = document.querySelector("#calculator-screen");
+
+const allBtns = document.querySelectorAll("button");
 
 const numberBtns = document.querySelectorAll(".btn.number");
+const clearBtn = document.querySelector("#clear");
+const delBtn = document.querySelector("#delete");
 
-numberBtns.forEach(
+const equalBtn = document.querySelector("#equal");
+
+function simpleCalc(string) {
+    const operators = ["+", "-", "x", "÷"];
+    let stringOperator = "";
+
+    for (const operator of operators) {
+        if (string.includes(operator)) {
+            stringOperator = operator;
+            break;
+        }
+    }
+
+    // The previous loop is equivalent to this code => const stringOperator = operators.find(op => string.includes(op));
+
+    const [leftValue, rightValue] = string.split(stringOperator).map(value => Number(value));
+
+    switch (stringOperator) {
+        case "+": return leftValue + rightValue;
+        case "-": return leftValue - rightValue;
+        case "x": return leftValue * rightValue;
+        case "÷": return leftValue / rightValue;
+        default: return "Unable to perform expression, unknown operator."
+    }
+}
+
+allBtns.forEach(
     button => button.addEventListener("click", () => {
-        resultScreen.textContent = button.innerHTML;
+        if (button.classList.contains("number") || button.classList.contains("operation")) {
+            screen.textContent += button.textContent;
+        }
     })
 );
+
+clearBtn.addEventListener("click", () => {screen.textContent = ""});
+
+delBtn.addEventListener("click", () => {
+    const stringArray = screen.textContent.split("");
+    stringArray.pop();
+    screen.textContent = stringArray.join("");
+});
+
+equalBtn.addEventListener("click", () => { 
+    screen.textContent = simpleCalc(screen.textContent); 
+});
